3de165711932f0c53467ab811a0d6d5a26716611
Toine Hartman <toine.hartman@swat.engineering>
2025-09-04 16:24:58 +0200

    Fall back to DEBUG just in case.

--- rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/BaseWorkspaceService.java
+++ rascal-lsp/src/main/java/org/rascalmpl/vscode/lsp/BaseWorkspaceService.java
@@ -189,7 +189,7 @@ abstract public class BaseWorkspaceService implements WorkspaceService, Language
                 final var extension = settings.get("extension").getAsJsonObject();
                 if (extension.has("minLogLevel")) {
                     final var minLogLevel = extension.get("minLogLevel").getAsString();
-                    Configurator.setRootLevel(Level.getLevel(minLogLevel));
+                    Configurator.setRootLevel(Level.toLevel(minLogLevel, Level.DEBUG));
                 }
             }
         }
